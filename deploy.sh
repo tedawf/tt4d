5e21c368392292973c215301bbf1059fb31c1e5f
tedawf <hello@tedawf.com>
2025-04-29 23:40:34 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo "--- Starting deployment for tt4d ---"
+
+echo "Getting latest changes"
+git pull origin main
+
+echo "Activating virtualenv"
+source .venv/bin/activate || { echo "Failed to activate virtualenv"; exit 1; }
+
+echo "Installing/updating python deps"
+pip install -r requirements.txt
+
+echo "Running db migrations"
+alembic upgrade head
+
+echo "Deactivating virtualenv"
+deactivate
+
+echo "Restarting tt4d service"
+sudo systemctl restart tt4d-api.service
+
+echo "Checking service status..."
+sleep 1
+sudo systemctl status tt4d-api.service --no-pager
+
+echo "--- Finished deployment for tt4d ---"
