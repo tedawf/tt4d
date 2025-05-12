6485cdb75453a86aef021bc85f1e112f83acf4b1
tedawf <hello@tedawf.com>
2025-05-12 14:31:56 +0800

    show user

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,17 @@ DEPLOY_TARGET="${1}"
 
 echo "--- Starting deployment for tt4d ---"
 
-echo "Fetching latest changes"
-git fetch origin
-
 if [ -z "${DEPLOY_TARGET}" ]; then
   DEPLOY_TARGET="origin/main"
   echo "No deployment commit provided, defaulting to ${DEPLOY_TARGET}"
 fi
+
+echo "Deploying commit: ${DEPLOY_TARGET}"
+echo "Current user: $(whoami)"
+echo "Working directory: $(pwd)"
+
+echo "Fetching latest changes"
+git fetch origin
 
 echo "Resetting workspace to ${DEPLOY_TARGET}"
 git reset --hard "${DEPLOY_TARGET}"
